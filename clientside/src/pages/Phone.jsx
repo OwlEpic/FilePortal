49b5d9424132c56e529f436836417a549a049b1b
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2023-04-29 01:42:09 -0400

    phone

---
+++ clientside/src/pages/Phone.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Phone = () => {
+  return (
+    <div>
+      <form>
+        <label for="fname">First name:</label>
+        <input type="text" id="fname" name="fname"></input><br></br>
+      </form>
+    </div>
+  )
+}
+
+export default Phone
