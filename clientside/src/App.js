9f6f98f7609299f76e0a49ee0ffd099fea255d2d
Mikaelsid <mikaelsid2315@gmail.com>
2023-04-28 22:38:27 -0400

    wooohooo

---
+++ clientside/src/App.js
@@ -0,0 +1,23 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+import "./style.scss"
+
+// <RouterProvider router={router} /> for later :)
+function App() {
+  return (
+    <div>
+      <div className="app">
+        <div className="container">
+          
+          hello World
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
