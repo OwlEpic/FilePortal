0a72a8023292099cf1faf40cb1563b0f4de1b9ff
Mikaelsid <mikaelsid2315@gmail.com>
2023-04-29 01:40:15 -0400

    pushikng

--- clientside/src/pages/Single.jsx
+++ clientside/src/pages/Single.jsx
@@ -1,6 +1,19 @@
 import { React, useState } from 'react'
+import { FileUploader } from "react-drag-drop-files";
+
 
 const Single = () => {
+
+  const fileTypes = ["JPG", "PNG", "GIF"];
+
+  const [file, setFile] = useState(null);
+  const handleChangeFile = (file) => {
+    setFile(file);
+    const reader = new FileReader()
+
+    reader.readAsBinaryString(file)
+    reader.onloadend = (event) => {console.log(reader.result)};
+  };
 
   const [input, setInput] = useState("")
 
@@ -18,6 +31,7 @@ const Single = () => {
       <input onChange={e=>setInput(e.target.value)} placeholder="waiting on code..." />
       <button onClick={handleChange}> refresh </button>
       <span>code is {codes.code}</span>
+      <FileUploader handleChange={handleChangeFile} name="file" types={fileTypes} />
     </div>
   )
 }
