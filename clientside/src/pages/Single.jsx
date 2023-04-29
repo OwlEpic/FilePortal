f967a82384476ea6a3154ed7bc8a307161139a36
Mikaelsid <mikaelsid2315@gmail.com>
2023-04-29 01:02:57 -0400

    did some work on updating the dom for single page

--- clientside/src/pages/Single.jsx
+++ clientside/src/pages/Single.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const Single = () => {
+
+  const [input, setInput] = useState("")
+
+  const [codes, setCodes] = useState({
+    code:"waiting for code",
+  })
+
+  const handleChange = (e) => {
+    setCodes((prev)=>({...prev, code: input}))
+  }
+
   return (
-    <div>Single</div>
+    <div>
+      <h1> sample </h1>
+      <input onChange={e=>setInput(e.target.value)} placeholder="waiting on code..." />
+      <button onClick={handleChange}> refresh </button>
+      <span>code is {codes.code}</span>
+    </div>
   )
 }
 
